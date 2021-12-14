22a0b2e7581f5f90530c912a1ee72ee875cb43bb
ALasherasSus <75242448+ALasherasSus@users.noreply.github.com>
2021-12-14 12:58:48 +0100

    Base de proyecto y pantalla inicio de sesion

---
+++ app/src/main/java/com/elorrieta/retoadpsp_3_androidport/MainActivity.java
@@ -0,0 +1,63 @@
+package com.elorrieta.retoadpsp_3_androidport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText etUsuario;
+    private EditText etContra;
+    private SharedPreferences p;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        p = getSharedPreferences("sharedprefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = p.edit();
+        String usu = p.getString("usuario","");
+        String pass = p.getString("password","");
+        if (usu == "" && pass == "") {
+            editor.putString("usuario", "arki");
+            editor.putString("password", "darki");
+            editor.apply();
+        }
+
+        etUsuario=(EditText)findViewById(R.id.etUsuario);
+        etContra=(EditText)findViewById(R.id.etContra);
+    }
+
+    public void login(View v){
+
+        String usuario=etUsuario.getText().toString();
+        String password=etContra.getText().toString();
+        String cadena="";
+
+        p = getSharedPreferences("sharedprefs", Context.MODE_PRIVATE);
+        String usu2 = p.getString("usuario","");
+        String pass2 = p.getString("password", "");
+
+        if(!usu2.equals(usuario)){
+            cadena=getString(R.string.sesionFracasoUsuario);
+            Toast notificacion= Toast.makeText(this,cadena,Toast.LENGTH_LONG);
+            notificacion.show();
+        }else if(!pass2.equals(password)){
+            cadena=getString(R.string.sesionFracasoContra);
+            Toast notificacion= Toast.makeText(this,cadena,Toast.LENGTH_LONG);
+            notificacion.show();
+        }else{
+            //cadena=getString(R.string.sesionExito);
+            etContra.setText("");
+            Intent i = new Intent(this, MenuPrincipal.class );
+            startActivity(i);
+        }
+
+    }
+}
