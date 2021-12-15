0c45b986f71914653170094034a287315c5f6481
ALasherasSus <75242448+ALasherasSus@users.noreply.github.com>
2021-12-15 09:08:53 +0100

    Icono de la App añadido

--- app/src/main/java/com/elorrieta/retoadpsp_3_androidport/MenuPrincipal.java
+++ app/src/main/java/com/elorrieta/retoadpsp_3_androidport/MenuPrincipal.java
@@ -2,7 +2,9 @@ package com.elorrieta.retoadpsp_3_androidport;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuPrincipal extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MenuPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+    }
+
+    public void acercaDe(View v){
+        Intent i = new Intent(this, Registro.class );
+        startActivity(i);
     }
 }
