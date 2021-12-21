4901511e66ed193a93c67e2fa5e29e6a2450a0e1
ALasherasSus <75242448+ALasherasSus@users.noreply.github.com>
2021-12-21 12:22:54 +0100

    Posicion actual completada

--- app/src/main/java/com/elorrieta/retoadpsp_3_androidport/PosicionActual.java
+++ app/src/main/java/com/elorrieta/retoadpsp_3_androidport/PosicionActual.java
@@ -49,6 +49,18 @@ public class PosicionActual extends FragmentActivity implements OnMapReadyCallba
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
         LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
         Criteria criteria = new Criteria();
         String provider = service.getBestProvider(criteria, false);
